5b42f60530430da0ba8a0566ae708f32475a4d13
maxime beuil <maxime.beuil@algoan.com>
2021-08-19 20:07:16 +0200

    chore(css): install and configure tailwind

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,7 +13,13 @@ export const getStaticProps: GetStaticProps = async ({ locale }) => {
 const Home: NextPage = () => {
   const i18n = useI18n();
 
-  return <div>{i18n.t('home.greetings')}</div>;
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <h1 className="text-3xl sm:text-5xl text-gray-900">
+        {i18n.t('home.greetings')}
+      </h1>
+    </div>
+  );
 };
 
 export default Home;
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  mode: 'jit',
+  purge: ['./src/**/*.{jsx,tsx}'],
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
